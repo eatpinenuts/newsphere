5ed997239d72b5832a5ae976d1434c8409f4bb4f
bflatman <b.flatman@gmail.com>
2013-04-25 00:25:11 +0100

    listing tab urls

--- popup.js
+++ popup.js
@@ -2,21 +2,52 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
- 
- window.onload = function() {
+window.onload = function() {
     var BGPage = chrome.extension.getBackgroundPage();
     //BGPage.upload("Test");
 
-    for(var key in BGPage.articles){
+    var listRoot = document.getElementById('alternatives');
+
+    for (var key in BGPage.articles) {
         console.log(BGPage.articles[key]);
         // somewhere in your code, preferably outside of global scope
         var div = document.createElement('li');
         div.id = 'alternative_item';
-    
-        document.getElementById('alternatives').appendChild(div);
+
+        listRoot.appendChild(div);
         // assuming elements contains string of html with your elements
-        div.innerHTML = '<a href="'+BGPage.articles[key].url+'">'+ 
-                BGPage.articles[key].url +'</a>';
+        div.innerHTML = '<a href="' + BGPage.articles[key].url + '" target="_blank">' +
+                BGPage.articles[key].url + '</a>';
+
+        var desc = document.createElement('p');
+        desc.innerHTML = BGPage.articles[key].description;
+
+        listRoot.appendChild(desc);
     }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('altLink').addEventListener(
+            'click', addAlternativeClicked);
+});
+
+function addAlternativeClicked(e) {
+    console.log('got here');
+
+    chrome.tabs.getAllInWindow(null, function(tabs) {
+        // TODO: only do if null
+        var alts = document.getElementById('alts');
+        // 
+        for (var i = 0; i < tabs.length; i++) {
+            chrome.tabs.get(tabs[i].id, function(tab) {
+                // check if page is in problem state (new tab).
+                //alert(tab.url);
+                
+                var li = document.createElement('li');
+                alts.appendChild(li);
+                // assuming elements contains string of html with your elements
+                li.innerHTML = '<a href="#">' + tab.url + '</a>';   
+            });
+        }
+    });
 }
