a5bfb526a75ae5b886723abb986d9ee277458237
Billy Flatman <b.flatman@sharpcode.biz>
2013-10-03 23:04:29 +0100

    Initial commit. Not working since server upgrade to ssl.

---
+++ background.js
@@ -0,0 +1,100 @@
+var articles = null;
+
+var url = null;
+
+var pageGlobals = {
+    tab: null,
+    url: "http://sharpcode.biz/unite/result.json?url="
+};
+
+var icons = ["white-19.png", "black-19.png", "green-19.png", "red-19.png"];
+
+function getUrl(tab) {
+    return pageGlobals.url + url_domain(tab.url);
+}
+
+function getAlternativesRequest(callback) {
+    requestUrlCode(getUrl(pageGlobals.tab), 
+        function(response){
+            LDResponse(response, callback);
+        }
+    );
+}
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, change, tab) {
+    if (url_domain(tab.url) !== 'devtools') {
+        pageGlobals.tab = tab;
+        
+        getAlternativesRequest(function(){console.log('called back 1');});
+    }
+}
+
+function requestUrlCode(url, callback) {
+    var request = new XMLHttpRequest();
+    
+    if (request === null) {
+        console.log("Unable to create request");
+    } else {
+        request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                callback(request.responseText);
+                //LDResponse(request.responseText, tab);
+            }
+        };
+        
+        request.open("GET", url, true);
+        request.send(null);
+    }
+}
+
+function LDResponse(response, callback) {
+    // do stuff with the response
+    // JSON.parse does not evaluate the attacker's scripts.
+    try {
+        var resp = JSON.parse(response);
+        chrome.pageAction.setIcon({path: icons[resp.result], tabId: pageGlobals.tab.id}, function() {
+            //if (resp.isValid === true) {
+            // check articles are available.
+            if (typeof(resp['articles']) !== 'undefined') {
+                // extract articles from server response.
+                //extractAltarticles(resp.articles);
+                articles = resp.articles;
+            }else {
+                articles = null;
+            }
+            if (typeof(resp['url']) !== 'undefined') {
+
+                url = resp['url'];
+                console.log('url: ' + url);
+            }
+
+            chrome.pageAction.show(pageGlobals.tab.id);
+            
+            callback();
+        });
+    } catch (ex) {
+        console.log(ex);
+    }
+}
+
+function url_domain(data) {
+    var a = document.createElement('a');
+    a.href = data;
+    return a.hostname;
+}
+
+// listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+// listen for when page become active. Needed as new tabs arn't updating.
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    // how to fetch tab url using activeInfo.tabid
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+
+        if (url_domain(tab.url) !== 'devtools') {
+            pageGlobals.tab = tab;
+            
+            getAlternativesRequest(function(){console.log('called back 2');});
+        }
+    });
+});
---
+++ popup.js
@@ -0,0 +1,79 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var BGPage;
+var debug;
+
+window.onload = function() {
+    BGPage = chrome.extension.getBackgroundPage();
+
+    appendAlternatives();
+};
+
+function appendAlternatives() {
+    // get list element.
+    var listRoot = document.getElementById('alternatives');
+    // clear all list items.
+    listRoot.innerHTML = '';
+    
+    var alts = document.getElementById('alts');
+    alts.innerHTML = '';
+    
+    // loop through articles.
+    for (var key in BGPage.articles) {
+        // create new list item.
+        var div = document.createElement('li');
+        // append new list item to list.
+        listRoot.appendChild(div);
+        // set inner html for list item to a link to alternative site.
+        div.innerHTML = '<a href="http://' + BGPage.articles[key].url + '" target="_blank">' +
+                BGPage.articles[key].url + '</a>';
+
+        /*var desc = document.createElement('p');
+        desc.innerHTML = BGPage.articles[key].description;
+
+        listRoot.appendChild(desc);*/
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('altLink').addEventListener(
+            'click', addAlternativeClicked);
+});
+
+function addLinkClicked(e) {
+    //http://sharpcode.biz/unite/addalt.json?url=htt://sharpcode.biz&alt=www.test.com
+    debug = e;
+    
+    var requestUrl = 'http://sharpcode.biz/unite/addalt.json?url=' + BGPage.url + '&alt=' + e.toElement.innerText;
+    
+    BGPage.requestUrlCode(requestUrl, callback);
+}
+
+function callback(response){
+    console.log(response);
+    BGPage.getAlternativesRequest(function(){console.log('called back 3');
+    appendAlternatives();
+    });
+}
+
+function addAlternativeClicked(e) {
+
+    chrome.tabs.getAllInWindow(null, function(tabs) {
+        // TODO: only do if null
+        var alts = document.getElementById('alts');
+        // 
+        for (var i = 0; i < tabs.length; i++) {
+            chrome.tabs.get(tabs[i].id, function(tab) {  
+                var li = document.createElement('li');
+                alts.appendChild(li);
+                // assuming elements contains string of html with your elements
+                li.innerHTML = '<a href="#">' + BGPage.url_domain(tab.url) + '</a>';
+                
+                li.addEventListener(
+                    'click', addLinkClicked);
+            });
+        }
+    });
+}
